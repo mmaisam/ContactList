b29cd9adfc93c7c200a8944d1759a2ab2fbddbfa
Muhammad Maisam <mmaisam01@gmail.com>
2024-09-16 00:48:31 -0400

    created AddressBook class

---
+++ src/AddressBook.java
@@ -0,0 +1,14 @@
+// Importing utility classes
+import java.util.*;
+public class AddressBook {
+    private Deque<BuddyInfo> buddies;
+    AddressBook(){
+        buddies = new ArrayDeque<>();
+    }
+    public void addBuddy(BuddyInfo buddy){
+        buddies.add(buddy); // Adds buddy to the end
+    }
+    public void removeBuddy(BuddyInfo buddy){
+        buddies.remove(buddy); // Removes specific buddy
+    }
+}
